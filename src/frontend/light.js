3bf2ae731221d0a4b33bc87f4620cd5b3975e31d
Parker Smith <parkerbsmith@gmail.com>
2014-10-04 23:07:26 -0400

    Added frontend concept

---
+++ src/frontend/light.js
@@ -0,0 +1,21 @@
+function Light(name) {
+    var self = this;
+
+    self.name = name;
+    self.on = ko.observable(false);
+    self.locked = false;
+
+    this.toggle = function() {       
+        var status = self.on();
+        self.on(!status);
+        console.debug(name + " status: " + self.on());
+    }
+}
+
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+function trafficViewModel(lights) {
+    this.lights = $.map(lights, function(name) { return new Light(name) });
+}
+
+// Activates knockout.js
+ko.applyBindings(new trafficViewModel(["red", "orange", "green"]));
